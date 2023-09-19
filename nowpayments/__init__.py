0f7a8741301e7694ab766ab5989180e1c01c18c1
Tomaž Mesarec <tomaz.mesarec@protonmail.com>
2023-09-19 15:47:28 +0100

    Added create_invoice

--- nowpayments/__init__.py
+++ nowpayments/__init__.py
@@ -174,6 +174,58 @@ class NOWPayments:
             f'Error {resp.status_code}: {resp.json().get("message", "Not descriptions")}'
         )
 
+    def create_invoice(
+        self,
+        price_amount: float,
+        price_currency: str,
+        pay_currency: str,
+        **kwargs: Union[str, float, bool, int],
+    ) -> Dict:
+        """
+        Creates a payment link. With this method, the customer is required to follow the generated url to complete
+        the payment.
+
+        :param float price_amount: The fiat equivalent of the price to be paid in crypto.
+
+        :param str price_currency: The fiat currency in which the price_amount is specified.
+
+        :param str pay_currency: The crypto currency in which the pay_amount is specified.
+
+        :param float pay_amount: The amount that users have to pay for the order stated in crypto.
+
+        :param str ipn_callback_url: Url to receive callbacks, should contain "http" or "https".
+
+        :param str order_id: Inner store order ID.
+
+        :param str order_description: Inner store order description.
+
+        :param int purchase_id: Id of purchase for which you want to create a other payment.
+
+        :param str payout_address: Receive funds on another address.
+
+        :param str payout_currency: Currency of your external payout_address.
+
+        :param int payout_extra_id: Extra id or memo or tag for external payout_address.
+
+        :param bool fixed_rate: Required for fixed-rate exchanges.
+        """
+        endpoint = "invoice"
+        data = PaymentData(
+            price_amount=price_amount,
+            price_currency=price_currency,
+            pay_currency=pay_currency,
+            **kwargs,
+        )
+        url = self._get_url(endpoint)
+        resp = self._post_requests(
+            url, data=data.clean_data_to_dict(is_sandbox=self._IS_SANDBOX)
+        )
+        if resp.ok:
+            return resp.json()
+        raise HTTPError(
+            f'Error {resp.status_code}: {resp.json().get("message", "Not descriptions")}'
+        )
+
     def get_payment_status(self, payment_id: int) -> Any:
         """
         Get the actual information about the payment.
